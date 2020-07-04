f05118db23b38f58fde320bf29c13a524b9db94b
creatorpiyush <piyushanand.1221@gmail.com>
2020-07-04 18:52:41 +0530

    Hello World app

---
+++ hello_world/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!</h1>
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Hello World!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
